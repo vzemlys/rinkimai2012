e9ff3ae437bd6f03155775459df6d0695b90e41d
Vaidotas Zemlys <zemlys@gmail.com>
2012-10-19 11:23:37 +0300

    Pick full correlations. Add correlation graph.

--- 10code.R
+++ 10code.R
@@ -8,3 +8,10 @@ pick100topcor<-function(d) {
     mcr<-na.omit(melt(cr))
     mcr[order(abs(mcr[,3]),decreasing=TRUE)[1:100],]
 }
+
+nicecor <- function(d) {
+    cr <- cor(d[,-4:-1])
+    rownames(cr)<-names(d)[-4:-1]
+    colnames(cr)<-names(d)[-4:-1]
+    cr
+}
