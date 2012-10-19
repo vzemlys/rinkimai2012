ba9daf1a9d7fb0a33f9a0e0005d791cf34defd6e
Vaidotas Zemlys <zemlys@gmail.com>
2012-10-19 11:09:53 +0300

    Added top100 correlated pairs from each party.

---
+++ 10code.R
@@ -0,0 +1,10 @@
+require(plyr)
+require(reshape)
+pick100topcor<-function(d) {
+    cr <- cor(d[,-4:-1])
+    rownames(cr)<-names(d)[-4:-1]
+    colnames(cr)<-names(d)[-4:-1]
+    cr[lower.tri(cr,diag=TRUE)] <- NA
+    mcr<-na.omit(melt(cr))
+    mcr[order(abs(mcr[,3]),decreasing=TRUE)[1:100],]
+}
